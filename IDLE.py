aa4e25cc718ea9afd0b9d8141ec389626c14f5f3
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-10-04 09:51:19 +0800

    Update IDLE.py

--- IDLE.py
+++ IDLE.py
@@ -1,22 +1,46 @@
+#預設編碼 部分版本不支援需自行定義
 #coding:UTF-8 
 
-x = 'Hello'
-y = ", world!"
-z = 'aaa,"eee"'
-q = '''python 
+
+#字串的定義
+
+>>> x = 'Hello'
+>>> y = ", world!"
+>>> z = 'aaa,"eee"'
+>>> q = '''python 
        hello '''
 
+#容器(Container Type)
 
->>> 
-a = (1,2,3,4,) 
+#Tuple :以()建構
+# 有順序, 不可更改
+
+#list  :以[]建構
+# 有順序,可更改內容
+
+
+>>> a = (1,2,3,4,5) 
 #整數常數
 
->>>
-b = [a,b,c,5,9]
+>>> b = [a,b,c,5,9]
+Error!
+# a,b,c 變數處理, 需先定義 否則會Error
 
-a,b,c > #變數處理, 需先定義 否則會Error
+>>> c =('xyz')
+>>> b= ['a','b',c,5,9]   #變數C有定義 OK
 
->>>
-C =('xyz')
-b= ['a','b',c,5,9]   #變數C有定義 OK
 
+>>> x = { 1,2,3,4}
+>>> type(x)
+set
+
+>>> y = {'a': 123,'b': 456}
+>>> type(y)
+dict
+
+>>> y['a']
+123
+
+>>> x = {4,3,2,1} #對照33行
+>>> x == xxx
+True
