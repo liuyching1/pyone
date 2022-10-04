f9e2440ac3383dd14c822f891bb3e9855b6fa8d9
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-10-05 00:44:55 +0800

    Update P.py

--- P.py
+++ P.py
@@ -1,48 +1,49 @@
 #預設編碼 部分版本不支援需自行定義
 #coding:UTF-8 
 
+##資料型態
+#####################
 
 #字串的定義
-
->>> x = 'Hello'
->>> y = ", world!"
->>> z = 'aaa,"eee"'
->>> q = '''python 
+x = 'Hello'
+y = ", world!"
+z = 'aaa,"eee"'
+q = '''python 
        hello '''
 
 #容器(Container Type)
 
-#Tuple :以()建構
+#列表list :以[]建構
+#有順序,可更改內容
+[3,4,5]
+
+#Tuple    :以()建構
 # 有順序, 不可更改
+("Hi","Python")
 
-#list  :以[]建構
-# 有順序,可更改內容
-
-
->>> a = (1,2,3,4,5) 
-#整數常數
-
->>> b = [a,b,c,5,9]
+a = (1,2,3,4,5) 
+b = [a,b,c,5,9]
 Error!
-# a,b,c 變數處理, 需先定義 否則會Error
+# a,b,c 變數處理, 需先定義, 否則會Error
 
->>> c =('xyz')
->>> b= ['a','b',c,5,9]   #變數C有定義 OK
+c =('xyz')
+b= ['a','b',c,5,9]   #變數C有定義OK
 
-
->>> x = { 1,2,3,4}
->>> type(x)
+#集合Set  :以{}建構
+x = {1,2,3,4}
+type(x)
 set
 
->>> y = {'a': 123,'b': 456}
->>> type(y)
+#字典Dictionary {key:val} 建構
+y = {'a': 123,'b': 456}
+type(y)
 dict
 
->>> y['a']
+y['a']
 123
 
->>> x = {4,3,2,1} #對照33行
->>> x == xxx
+x = {4,3,2,1} #對照33行
+x == x
 True
 
 input() #回傳字串型態
